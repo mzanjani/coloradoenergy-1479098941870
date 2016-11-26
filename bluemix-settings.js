346d29a3f806c7c3888d8835b1b1376ae3f3d232
Mohammad Zanjani <mzanjani@au1.ibm.com>
2016-11-26 03:45:26 -0600

    Adding authentication

--- bluemix-settings.js
+++ bluemix-settings.js
@@ -47,6 +47,8 @@ var settings = module.exports = {
     // You can protect the user interface with a userid and password by using the following property
     // the password must be an md5 hash  eg.. 5f4dcc3b5aa765d61d8327deb882cf99 ('password')
     //httpAdminAuth: {user:"user",pass:"5f4dcc3b5aa765d61d8327deb882cf99"},
+    
+    adminAuth: require("./user-authentication");
 
     // Serve up the welcome page
     httpStatic: path.join(__dirname,"public"),
